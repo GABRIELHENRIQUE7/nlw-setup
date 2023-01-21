8ed133b77f83ed143f557b882df6bc664064bfdf
GABRIEL HENRIQUE <83559794+GABRIELHENRIQUE7@users.noreply.github.com>
2023-01-20 22:33:52 -0300

    update javascript

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-   const dayExists = nlwSetup.dayExists(today)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
-    if(dayExists) {
-          alert("Dia já incluso")
-          return
-       }
-    
-   alert("Adicionado com sucesso ✅")
-   nlwSetup.addDay(today)
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
-function save() {                               
-   localStorage.setItem("NLWSetup@habits", jSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
